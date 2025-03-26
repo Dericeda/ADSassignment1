76f393cac15cb6ea6534d9035a00c3b7f31ae7c0
Dericeda <nurasyl.asanov@mail.ru>
2025-03-26 13:25:03 +0500

    task4,5,6

---
+++ src/task7.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class task7 {
+
+    public static void reverseFunction(int n, Scanner input){
+        if (n == 0) {
+            return;
+        }
+
+        int x = input.nextInt();
+        reverseFunction(n-1,input);
+        System.out.print(x +" ");
+    }
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        reverseFunction(a, input);
+    }
+
+}
