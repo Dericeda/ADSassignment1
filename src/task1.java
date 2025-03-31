42c9bae05a62d9ccfe93112ca51c2b41e3452124
Dericeda <nurasyl.asanov@mail.ru>
2025-03-31 21:41:26 +0500

    task4,5,6

---
+++ src/task1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class task1 {
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int massiv[]= new int [a];
+
+        for(int i =1; i<a;i++){
+            int b = input.nextInt();
+            massiv[i] = b;
+        }
+
+        int minNumber = massiv[0];
+
+        for(int i= 0;i<a;i++){
+            if(minNumber>massiv[i]){
+                minNumber = massiv[i];
+            }
+        }
+
+        System.out.print(minNumber);
+    }
+}
